13ac7ea0437ca84754b40ff155e10a0df625dc96
SOURAV ROY <souravroy.info13@gmail.com>
2020-05-31 22:59:32 +0600

    Loop List by Index in range

--- Loop_List.py
+++ Loop_List.py
@@ -1,6 +1,11 @@
 grocery_item = ["Potato", "Tomato", "Water", "Ginger", "Onion"]
-for item in grocery_item:
-    if item == 'Water':
-        continue
-    print(item)
 print(grocery_item)
+
+# for item in grocery_item:
+#     if item == 'Water':
+#         break
+#     print(item)
+
+for i in range(0, len(grocery_item)):
+    print(grocery_item[i])
+print("***Finished***")
