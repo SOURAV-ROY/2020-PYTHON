974d1cebe8b4c77fae5bc61fd2f9402864028ea2
SOURAV ROY <souravroy.info13@gmail.com>
2020-05-31 23:36:00 +0600

    Text File Modify In List

--- File_Start.py
+++ File_Start.py
@@ -1,4 +1,9 @@
 with open("sourav.txt", mode="r") as s_file:
     for line in s_file.readlines():
-        print(line)
+        # print(line, end="")
+        # print(line.strip(), end="")
+        # words = line.split(" ")
+        striped_line = line.strip()
+        words = striped_line.split(" ")
+        print(words)
 print("***Finished***")
