b9465dfe4e6183d582c3d588bd4e65c50b036f60
SOURAV ROY <souravroy.info13@gmail.com>
2020-05-31 23:38:59 +0600

    All Words In One List

--- File_Start.py
+++ File_Start.py
@@ -1,9 +1,12 @@
 with open("sourav.txt", mode="r") as s_file:
+    all_words = []
     for line in s_file.readlines():
         # print(line, end="")
         # print(line.strip(), end="")
         # words = line.split(" ")
         striped_line = line.strip()
         words = striped_line.split(" ")
-        print(words)
+        # print(words)
+        all_words += words
+        print(all_words)
 print("***Finished***")
