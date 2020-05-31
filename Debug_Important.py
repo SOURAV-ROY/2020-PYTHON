a52698cfa48214e5f5b65dc716fb3168e72d65ac
SOURAV ROY <souravroy.info13@gmail.com>
2020-06-01 00:35:36 +0600

    Debuging for Loop why print same number

--- Debug_Important.py
+++ Debug_Important.py
@@ -1,5 +1,8 @@
-username = input("Your name: ")
-value = 10
-string_value = str(value)
-new_string = username + string_value
-print(new_string)
+# username = input("Your name: ")
+# value = 10
+# string_value = str(value)
+# new_string = username + string_value
+# print(new_string)
+for i in range(10):
+    print(i)
+print("***Finished***")
