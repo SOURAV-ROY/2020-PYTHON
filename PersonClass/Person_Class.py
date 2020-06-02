726724c23326192c99020f904935843e2af88ccd
SOURAV ROY <souravroy.info13@gmail.com>
2020-06-02 22:02:41 +0600

    Set Method Update input

--- PersonClass/Person_Class.py
+++ PersonClass/Person_Class.py
@@ -11,11 +11,15 @@ class Person:
     def get_name(self):
         return self.name
 
+    def set_name(self, new_name):
+        self.name = new_name
+
     def get_summery(self):
         return f"Name: {self.name}, DOB: {self.date_of_birth}, Height: {self.height}"
 
 
 method_name()
 person_summery = Person("First Person", "1996", "6 feet")
-
+print(person_summery.get_summery())
+person_summery.set_name("Sourav Chandra Roy")
 print(person_summery.get_summery())
