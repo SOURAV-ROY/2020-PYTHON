7b0829072288c66e7f2ed8503020ffe448f30abf
SOURAV ROY <souravroy.info13@gmail.com>
2020-06-02 22:20:56 +0600

    Name can't have any 0 number

--- PersonClass/Person_Class.py
+++ PersonClass/Person_Class.py
@@ -12,7 +12,13 @@ class Person:
         return self.name
 
     def set_name(self, new_name):
+        if self._has_any_number(new_name):
+            print("Sorry Person Can't have any number")
+            return
         self.name = new_name
+
+    def _has_any_number(self, string):
+        return "0" in string
 
     def get_summery(self):
         return f"Name: {self.name}, DOB: {self.date_of_birth}, Height: {self.height}"
@@ -21,5 +27,9 @@ class Person:
 method_name()
 person_summery = Person("First Person", "1996", "6 feet")
 print(person_summery.get_summery())
+
 person_summery.set_name("Sourav Chandra Roy")
+print(person_summery.get_summery())
+
+person_summery.set_name('111100000111Sourav')
 print(person_summery.get_summery())
