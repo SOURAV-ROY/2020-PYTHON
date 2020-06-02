7df38b73c63bcaa24b04b7511543bb69fea3234f
SOURAV ROY <souravroy.info13@gmail.com>
2020-06-02 21:55:58 +0600

    Get Summery by 1 Method

--- PersonClass/Person_Class.py
+++ PersonClass/Person_Class.py
@@ -3,16 +3,19 @@ def method_name():
 
 
 class Person:
-    def __init__(self, person_name):
+    def __init__(self, person_name, date_of_birth, ht):
         self.name = person_name
+        self.date_of_birth = date_of_birth
+        self.height = ht
 
     def get_name(self):
         return self.name
 
+    def get_summery(self):
+        return f"Name: {self.name}, DOB: {self.date_of_birth}, Height: {self.height}"
+
 
 method_name()
-a_person = Person('First Person')
-b_person = Person('Second Person')
+person_summery = Person("First Person", "1996", "6 feet")
 
-print(a_person.get_name())
-print(b_person.get_name())
+print(person_summery.get_summery())
