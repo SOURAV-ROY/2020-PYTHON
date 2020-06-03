c9f185c476a7d154c8098bffd5808df738157f5f
SOURAV ROY <souravroy.info13@gmail.com>
2020-06-03 23:34:04 +0600

    String Representation by __repr__ ✔

--- PersonClass/Person_Class.py
+++ PersonClass/Person_Class.py
@@ -70,7 +70,10 @@ class Student(Person):
         # return f'Name: {self.get_name()}, ID: {self.id}, Email: {self.email}, Birth: {self.get_year_of_birth()}'
         return f"Name: {self.get_name()}, Email: {str(self.email)}, Birth: {self.get_year_of_birth()}"
 
-    def __str__(self):
+    # def __str__(self):
+    #     return f"Name: {self.get_name()}, ID: {self.id} Email: {self.email}, Birth: {self.get_year_of_birth()}"
+
+    def __repr__(self):
         return f"Name: {self.get_name()}, ID: {self.id} Email: {self.email}, Birth: {self.get_year_of_birth()}"
 
 
