e86ef219e05761a8b4bddc2b22c6ea655651c851
SOURAV ROY <souravroy.info13@gmail.com>
2020-06-15 16:15:21 +0600

    LinkedList With Array & Bonus Flag ✔💚

---
+++ LinkedList_Array/LinkedList.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, value):
+        self.next = None
+        self.prev = None
+        self.val = value
+
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def add(self, val):
+        node = Node(val)
+        if self.tail is None:
+            self.head = node
+            self.tail = node
+            self.size += 1
+        else:
+            self.tail.next = node
+            node.prev = self.head
+            self.tail = node
+            self.size += 1
+
+    def __str__(self):
+        vals = []
+        node = self.head
+        while node is not None:
+            vals.append(node.val)
+            node = node.next
+        return f"[{', '.join(str(val) for val in vals)}]"
+
+
+my_list = DoubleLinkedList()
+my_list.add(1)
+my_list.add(5)
+my_list.add(2)
+my_list.add(2)
+print(my_list)
