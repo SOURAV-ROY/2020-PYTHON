61b6908d6fa7c62a1751f5796e9a202218bacea0
SOURAV ROY <souravroy.info13@gmail.com>
2020-07-04 22:14:13 +0600

    BST Work Awesome

--- DataStructure_Algorithm/Tree.py
+++ DataStructure_Algorithm/Tree.py
@@ -37,7 +37,43 @@ class BinaryTree:
         return tree_printer.get_tree_string(self.root)
 
 
-my_tree = BinaryTree()
-for c in ['a', 'b', 'c', 'd', 'e', 'f']:
-    my_tree.insert(c)
-    print(my_tree)
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def __insert_value(self, node, value):
+        if node is None:
+            return
+        if node.val == value:
+            return
+        elif value < node.val:
+            if node.left is None:
+                node.left = TreeNode(value)
+                return
+            self.__insert_value(node.left, value)
+        else:
+            if node.right is None:
+                node.right = TreeNode(value)
+                return
+            self.__insert_value(node.right, value)
+
+    def insert(self, val):
+        if self.root is None:
+            self.root = TreeNode(val)
+        else:
+            self.__insert_value(self.root, val)
+
+    def __str__(self):
+        tree_printer = BinaryTreePrinter()
+        return tree_printer.get_tree_string(self.root)
+
+
+# my_tree = BinaryTree()
+# for c in ['a', 'b', 'c', 'd', 'e', 'f', 'g']:
+#     my_tree.insert(c)
+#     print(my_tree)
+
+bst = BST()
+for i in [4, 3, 2, 1, 55, 5, 34, 3]:
+    bst.insert(i)
+    print(bst)
